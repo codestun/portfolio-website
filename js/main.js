c006c2169fd36ed9fd3abdea098e7f131c00f677
codestun <doerthy.k@gmail.com>
2024-04-14 19:24:50 +0200

    Refactor JS/CSS for nav links & hamburger toggle

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var hamburger = document.getElementById('hamburger-icon');
-  var nav = document.querySelector('.main-navigation');
+  let hamburger = document.getElementById('hamburger-icon');
+  let nav = document.querySelector('.main-navigation');
 
   // Function to toggle navigation display
   function toggleNav() {
     nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+    hamburger.classList.toggle('is-active'); // Toggle 'is-active' class on hamburger icon
   }
 
   // Toggle navigation on hamburger click
@@ -25,4 +26,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Initial adjustment in case the page loads on a non-default size
   adjustNavForWindowSize();
+
+  let navLinks = document.querySelectorAll('.navigation-list__item'); // Select all navigation links
+
+  // Function to remove existing active classes
+  function removeActiveClasses() {
+    navLinks.forEach(function (link) {
+      link.classList.remove('navigation-list__item--active');
+    });
+  }
+
+  // Function to set a link as active
+  function setActive(link) {
+    removeActiveClasses();
+    link.classList.add('navigation-list__item--active');
+  }
+
+  // Add click event listeners to navigation links
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      setActive(link);
+      // Close the navigation menu if open ( useful for mobile view)
+      if (window.innerWidth <= 768) {
+        toggleNav();
+      }
+    });
+  });
+
+  // Code to ensure the correct link is active on page load or refresh
+  let currentPath = window.location.hash; // get current hash from URL
+  if (currentPath) {
+    let activeLink = document.querySelector(`a[href='${currentPath}']`);
+    if (activeLink) {
+      setActive(activeLink);
+    }
+  }
+
 });
+
